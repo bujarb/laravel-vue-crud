52893735442f6a857141b314966badf17be00374
Bujar Begisholli <begisholli.bujar@gmail.com>
2018-01-04 21:57:00 +0100

    commit

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,17 @@
+// App.vue
+
+<template>
+    <div class="container">
+        <div>
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+    }
+</script>
